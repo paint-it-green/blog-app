051e6355603ff43834002224ad5645a92e093fae
grnpntr <gerwin@hyperstacksinc.com>
2021-03-26 08:46:13 +0800

    feat: add testing

--- src/app/pages/article/article.component.spec.ts
+++ src/app/pages/article/article.component.spec.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from "@angular/compiler";
+import { RouterTestingModule } from "@angular/router/testing";
+import { BsModalRef, ModalModule } from "ngx-bootstrap/modal";
 
 import { ArticleComponent } from "./article.component";
 
@@ -8,7 +12,16 @@ describe("ArticleComponent", () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ArticleComponent]
+      declarations: [ArticleComponent],
+      schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        BsModalRef
+      ],
+      imports: [
+        RouterTestingModule.withRoutes([]),
+        ModalModule.forRoot(),
+        HttpClientTestingModule,
+      ],
     })
       .compileComponents();
   });
--- src/app/pages/article/detail/detail.component.spec.ts
+++ src/app/pages/article/detail/detail.component.spec.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { NO_ERRORS_SCHEMA } from "@angular/compiler";
+import { RouterTestingModule } from "@angular/router/testing";
+import { BsModalRef, ModalModule } from "ngx-bootstrap/modal";
 
 import { DetailComponent } from "./detail.component";
 
@@ -8,7 +12,16 @@ describe("DetailComponent", () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [DetailComponent]
+      declarations: [DetailComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        BsModalRef
+      ],
+      imports: [
+        RouterTestingModule.withRoutes([]),
+        ModalModule.forRoot(),
+        HttpClientTestingModule,
+      ],
     })
       .compileComponents();
   });
